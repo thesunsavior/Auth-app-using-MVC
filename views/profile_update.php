aa6699bbaf7c27118ff23f1a66c5b4b890b49554
Trung <20trung.pq@vinuni.edu.vn>
2022-07-03 03:08:19 +0700

    update profile

--- views/profile_update.php
+++ views/profile_update.php
@@ -5,13 +5,19 @@
 use thecodeholic\phpmvc\form\Form;
 use thecodeholic\phpmvc\Application;
 
+error_log("After POSTEE");
+
+error_log(sizeof($_FILES));
+error_log(sizeof($_POST));
+
+
 ?>
 
 <h1>Update your infomation</h1>
 <div class="row">
     <img src="/media/<?php echo Application::$app->user->img_name ?>" alt="Avatar pic" style="width: 200px; height: 200px; vertical-align: middle; border-radius:50%;"><img>
     <div class="col">
-        <?php $form = Form::begin('', 'post') ?>
+        <?php $form = Form::begin('', 'POST') ?>
         <?php echo $form->field($model, 'img_name', 'file') ?>
         <div class="row">
             <div class="col">
@@ -22,7 +28,7 @@ use thecodeholic\phpmvc\Application;
             </div>
         </div>
         <?php echo $form->field($model, 'email', 'text') ?>
-        <button class="btn btn-success">Submit</button>
+        <button class="btn btn-success" name="submit">Submit</button>
         <?php Form::end() ?>
     </div>
 </div>
